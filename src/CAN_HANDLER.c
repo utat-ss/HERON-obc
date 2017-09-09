eb6948fc7ec874092216266e88ca854db4f27565
shrey-p <shreysp23@hotmail.com>
2017-09-09 18:09:48 -0400

    changed minor syntax

--- src/CAN_HANDLER.c
+++ src/CAN_HANDLER.c
@@ -1,46 +1,45 @@
-// Returns 0 if no command executed 
+// Returns 0 if no command executed
 
-int CAN_HANDLER ( uint16_t identifier, uint8_t* pt_data, uint8_t size) {
+int can_handler (uint16_t identifier, uint8_t* pt_data, uint8_t size) {
 
 	// Get 11 bit identifier (priority)
 
-	// Get data 
+	// Get data
 
-	// Switch statements 
+	// Switch statements
 
-	uint16_t command; 
+	uint16_t command;
 
 	uint16_t sender_id = identifier & TX_MASK;
 	uint16_t receiver_id = identifier & RX_MASK;
 	uint16_t msg_id = identifier & MSG_MASK;
 
-	command = identifier; 
+	command = identifier;
 
 	// Predefinined commands
-	switch(command) 
+	switch(command)
 	{
 
 		case command_1:
-		// code to be executed 
+		// code to be executed
 
 			break;
 
 		case command_2:
-		// code to be executed 
+		// code to be executed
 
 			break;
 
 		case command_3:
-		// code to be executed 
+		// code to be executed
 
 			break;
 
-		default: 
+		default:
 
-			return 0; 
-			//Return error no command executed 
+			return 0;
+			//Return error no command executed
 	}
 
 
 }
-
