575753ca76418e2676e253186bf4432354bb968e
alihaydaroglu <ali.haydaroglu@mail.utoronto.ca>
2018-01-02 16:17:15 -0500

    Cosmetic changes to includes

--- src/obc.h
+++ src/obc.h
@@ -1,16 +1,24 @@
 #ifndef OBC_H
 #define OBC_H
 
-#include 	<avr/io.h>
-#include 	<util/delay.h>
+
+// standard C ;ibraries
 #include  	<stdbool.h>
 #include  	<stdint.h>
 
+// avr-libc includes
+#include 	<avr/io.h>
+#include 	<util/delay.h>
+
+// lib-common includes
 #include 	<spi/spi.h>
 #include 	<uart/uart.h>
 #include 	<uart/log.h>
 #include 	<can/can.h>
 
+// project file includes
 #include 	"rtc.h"
 #include 	"mem.h"
+
+
 #endif
