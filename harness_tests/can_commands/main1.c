20a61919d7a821610c27d1f29c77d70ba657c084
R-Song <ryansong02@gmail.com>
2019-04-17 17:54:59 -0400

    Finished writing test for pay-hk

--- harness_tests/can_commands/main1.c
+++ harness_tests/can_commands/main1.c
@@ -11,7 +11,7 @@ void harness_func_test( void ){
 	ASSERT_EQUAL(1+1, 2);
 }
 
-// check functionality of processing rx messages for a variety of cases
+// test functionality of eps hk
 void eps_hk_test( void ){
 	// 8 byte data array for peeking into queues
 	uint8_t data[8] = { 0 };
@@ -29,6 +29,7 @@ void eps_hk_test( void ){
 			peek_queue(eps_tx_msg_queue, data);
 			// check if message is from the right source
 			ASSERT_EQUAL(data[1], CAN_EPS_HK);
+			// send the message to fetch next field from eps
 			send_next_eps_tx_msg();
 		}
 		if(!queue_empty(data_rx_msg_queue)){
@@ -48,17 +49,55 @@ void eps_hk_test( void ){
 
 	// after all fields are recieved, check if eps_hk_data matches global array
 	for(uint8_t i=0; i<CAN_EPS_HK_FIELD_COUNT; i++){
-		ASSERT_EQUAL(eps_hk_test_data[i], eps_hk_fields[i])
+		ASSERT_EQUAL(eps_hk_test_data[i], eps_hk_fields[i]);
 	}
 }
 
+// test functionality of pay hk
+void pay_hk_test( void ){
+	// 8 byte data array for peeking into queues
+	uint8_t data[8] = { 0 };
+	// array holding all the data recieved from the fields
+	uint32_t pay_hk_test_data[CAN_PAY_HK_FIELD_COUNT] = { 0 };
+	// number of fields recieved
+	uint8_t field_number = 0;
 
+	// request hk data from eps
+	req_eps_hk_fn();
 
+	// recieve and request each successive field until completion
+	while(field_number < CAN_PAY_HK_FIELD_COUNT) {
+		if(!queue_empty(eps_tx_msg_queue)){
+			peek_queue(eps_tx_msg_queue, data);
+			// check if message is from the right source
+			ASSERT_EQUAL(data[1], CAN_PAY_HK);
+			// send the message to fetch next field from eps
+			send_next_pay_tx_msg();
+		}
+		if(!queue_empty(data_rx_msg_queue)){
+			peek_queue(data_rx_msg_queue, data);
+			// check if message is from the right source
+			ASSERT_EQUAL(data[1], CAN_PAY_HK);
+			// save the data read for later reference
+			pay_hk_test_data[field_number] =
+                (((uint32_t) data[3]) << 16) |
+                (((uint32_t) data[4]) << 8) |
+                ((uint32_t) data[5]);
+            // process the next rx_msg
+            process_next_rx_msg();
+			field_number++;
+		}
+	}
 
-
+	// after all fields are recieved, check if eps_hk_data matches global array
+	for(uint8_t i=0; i<CAN_PAY_HK_FIELD_COUNT; i++){
+		ASSERT_EQUAL(pay_hk_test_data[i], pay_hk_fields[i]);
+	}
+}
 
 test_t t1 = { .name = "harness functionality test", .fn = harness_func_test };
-test_t t2 = { .name = "", .fn = };
+test_t t2 = { .name = "eps hk test", .fn = eps_hk_test };
+test_t t3 = { .name = "pay hk test", .fn = pay_hk_test };
 
 test_t* suite[] = { &t1, &t2, &t3, &t4, &t5, &t6, &t7, &t8, &t9 };
 
