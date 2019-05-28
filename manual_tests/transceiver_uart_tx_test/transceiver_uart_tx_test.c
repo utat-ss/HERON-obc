d94d2e2a4e63f2c3f28b4522b63f80d3764d346f
Bruno Almeida <brunofalmeida@users.noreply.github.com>
2019-05-28 00:03:23 -0400

    Added transceiver CRC test, updated UART TX test

--- manual_tests/transceiver_uart_tx_test/transceiver_uart_tx_test.c
+++ manual_tests/transceiver_uart_tx_test/transceiver_uart_tx_test.c
@@ -17,13 +17,31 @@ Other Notes:
 #include "../../src/rtc.h"
 #include "../../src/transceiver.h"
 
-void add_message(char* string) {
+void add_standard_message(uint8_t msg_type, uint32_t arg1, uint32_t arg2) {
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        trans_tx_dec_msg[0] = msg_type;
+        trans_tx_dec_msg[1] = (arg1 >> 24) & 0xFF;
+        trans_tx_dec_msg[2] = (arg1 >> 16) & 0xFF;
+        trans_tx_dec_msg[3] = (arg1 >> 8) & 0xFF;
+        trans_tx_dec_msg[4] = arg1 & 0xFF;
+        trans_tx_dec_msg[5] = (arg2 >> 24) & 0xFF;
+        trans_tx_dec_msg[6] = (arg2 >> 16) & 0xFF;
+        trans_tx_dec_msg[7] = (arg2 >> 8) & 0xFF;
+        trans_tx_dec_msg[8] = arg2 & 0xFF;
+
+        trans_tx_dec_msg_len = 9;
+        trans_tx_dec_msg_avail = true;
+    }
+}
+
+void add_string_message(char* string) {
     ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
         // Don't add the terminating '\0' character
-        trans_tx_dec_msg_len = strlen(string);
-        for (uint8_t i = 0; i < trans_tx_dec_msg_len; i++) {
+        uint8_t len = strlen(string);
+        for (uint8_t i = 0; i < len; i++) {
             trans_tx_dec_msg[i] = string[i];
         }
+        trans_tx_dec_msg_len = len;
         trans_tx_dec_msg_avail = true;
     }
 }
@@ -46,8 +64,20 @@ void print_encoded(void) {
     print_bytes((uint8_t*) trans_tx_enc_msg, trans_tx_enc_msg_len);
 }
 
-void test_message(char* string) {
-    add_message(string);
+void test_standard_message(uint8_t msg_type, uint32_t arg1, uint32_t arg2) {
+    add_standard_message(msg_type, arg1, arg2);
+    print_decoded();
+    encode_trans_tx_msg();
+    print_encoded();
+    send_trans_tx_enc_msg();
+    print("\n");
+    print_decoded();
+    print_encoded();
+    print("\n\n");
+}
+
+void test_string_message(char* string) {
+    add_string_message(string);
     print_decoded();
     encode_trans_tx_msg();
     print_encoded();
@@ -61,16 +91,19 @@ void test_message(char* string) {
 int main(void) {
     init_uart();
     print("\n\n");
-
     init_uptime();
 
     print("Starting test\n\n");
 
     init_trans_uart();
 
-    test_message("hello");
-    test_message("world!");
-    test_message("UTAT");
+    test_string_message("hello");
+    test_string_message("world!");
+    test_string_message("UTAT");
+
+    test_standard_message(0, 0, 0);
+    test_standard_message(12, UINT32_MAX, 5);
+    test_standard_message(UINT8_MAX, 17, UINT32_MAX);
 
     print("Done test\n");
 }
