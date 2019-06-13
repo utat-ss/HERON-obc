80acf0796768a58eb3885ee07a69d5be521d81bb
Bruno Almeida <brunofalmeida@users.noreply.github.com>
2019-06-12 20:57:28 -0400

    Started pre launch config program

---
+++ manual_tests/pre_launch_config/pre_launch_config.c
@@ -0,0 +1,24 @@
+/*
+A program to configure OBC before launch (e.g. erase memory, check EEPROM erased,
+set RTC to date/time 0).
+*/
+
+#include "../../src/antenna.h"
+
+void config_comms_delay_eeprom(void) {
+    eeprom_write_dword(COMMS_DELAY_DONE_EEPROM_ADDR, EEPROM_DEF_DWORD);
+}
+
+// Comment/uncomment functionality as desired
+int main(void){
+    init_uart();
+
+    print("\n\n");
+    print("Starting pre launch configuration\n");
+
+    config_comms_delay_eeprom();
+   
+    print("Done pre launch configuration\n");
+
+    return 0;
+}
