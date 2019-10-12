49cfdd7b0f1ac9afeb53055dfb3353d20f8cdec7
XiuLuoShen <zengzeng419@gmail.com>
2019-10-12 13:42:06 -0400

    Updated tests and ran on OBC to ensure they work as expected

--- harness_tests/commands/main1.c
+++ harness_tests/commands/main1.c
@@ -21,30 +21,32 @@
  */
 void basic_commands_test (void) {
     // Set the date to yr = 255, month = 4, date = 1, disregard time
-    enqueue_cmd(&set_rtc_cmd, 0xff0401, 0); 
+    enqueue_cmd(&set_rtc_cmd, 0x210401, 0); 
     enqueue_cmd(&get_rtc_cmd, 0, 0);
     
     execute_next_cmd();
-    ASSERT_EQ(RTC_MIN_R, 0);
-    ASSERT_EQ(RTC_HOUR_R, 0);
-    ASSERT_EQ(RTC_WKDAY_R, 0);
-    ASSERT_EQ(RTC_DAY_R, 1);
-    ASSERT_EQ(RTC_MONTH_R, 4);
-    ASSERT_EQ(RTC_YEAR_R, 0xff);
+    rtc_date_t date = read_rtc_date();
+    rtc_time_t time = read_rtc_time();
+    ASSERT_EQ(time.ss, 0);  // This could fail
+    ASSERT_EQ(time.mm, 0);
+    ASSERT_EQ(time.hh, 0);
+    ASSERT_EQ(date.dd, 1);
+    ASSERT_EQ(date.mm, 4);
+    ASSERT_EQ(date.yy, 0x21);
 
-    ASSERT_FALSE(trans_tx_dec_avail);
+    ASSERT_TRUE(trans_tx_dec_avail);
     
     execute_next_cmd();
     ASSERT_TRUE(trans_tx_dec_avail);
     if (trans_tx_dec_avail) {
-        ASSERT_EQ(trans_tx_dec_len, 6);
+        ASSERT_EQ(trans_tx_dec_len, 15);
         if (trans_tx_dec_len == 6) {
-            // ignore the seconds
-            ASSERT_EQ(trans_tx_dec_msg[1], 0);
-            ASSERT_EQ(trans_tx_dec_msg[2], 0);
-            ASSERT_EQ(trans_tx_dec_msg[3], 1);
-            ASSERT_EQ(trans_tx_dec_msg[4], 4);
-            ASSERT_EQ(trans_tx_dec_msg[5], 0xff);
+            ASSERT_EQ(trans_tx_dec_msg[9], 0);  // This might fail
+            ASSERT_EQ(trans_tx_dec_msg[10], 0);
+            ASSERT_EQ(trans_tx_dec_msg[11], 0);
+            ASSERT_EQ(trans_tx_dec_msg[12], 1);
+            ASSERT_EQ(trans_tx_dec_msg[13], 4);
+            ASSERT_EQ(trans_tx_dec_msg[14], 0xff);
         }
     }
 
@@ -53,7 +55,7 @@ void basic_commands_test (void) {
     execute_next_cmd();
     ASSERT_TRUE(trans_tx_dec_avail);
     if (trans_tx_dec_avail){
-        ASSERT_EQ(trans_tx_dec_len, 12);
+        ASSERT_EQ(trans_tx_dec_len, 21);
     }
 
     // Read the recent status info, just confirm length, content doesn't matter
@@ -61,7 +63,7 @@ void basic_commands_test (void) {
     execute_next_cmd();
     ASSERT_TRUE(trans_tx_dec_avail);
     if (trans_tx_dec_avail) {
-        ASSERT_EQ(trans_tx_dec_len, 27);
+        ASSERT_EQ(trans_tx_dec_len, 42);    // OBC has 5 field --> 33 bytes in total + 9
     }
 
     // Test get and set for data collection, data period
@@ -70,47 +72,47 @@ void basic_commands_test (void) {
 
     execute_next_cmd();
     ASSERT_EQ(obc_hk_auto_data_col.period, 40);
-    ASSERT_FALSE(trans_tx_dec_avail);
+    ASSERT_TRUE(trans_tx_dec_avail);
 
     execute_next_cmd();
     ASSERT_TRUE(trans_tx_dec_avail);
     if (trans_tx_dec_avail) {
-        ASSERT_EQ(trans_tx_dec_len, 1); // Assuming size of period var is 1 byte
-        ASSERT_EQ(trans_tx_dec_msg[0], 40);
+        ASSERT_EQ(trans_tx_dec_len, 13); // Assuming size of period var is 1 byte
+        ASSERT_EQ(trans_tx_dec_msg[12], 40);
     }
 
     // Enable the data collection
     enqueue_cmd(&set_auto_data_col_enable_cmd, 1, 1);
     execute_next_cmd();
-    ASSERT_FALSE(trans_tx_dec_avail);
+    ASSERT_TRUE(trans_tx_dec_avail);
     ASSERT_TRUE(obc_hk_auto_data_col.enabled);
     enqueue_cmd(&get_auto_data_col_enable_cmd, 1, 0);
     execute_next_cmd();
     ASSERT_TRUE(trans_tx_dec_avail);
     if (trans_tx_dec_avail) {
-        ASSERT_EQ(trans_tx_dec_len, 1);
-        ASSERT_EQ(trans_tx_dec_msg[0], 1);
+        ASSERT_EQ(trans_tx_dec_len, 10);
+        ASSERT_EQ(trans_tx_dec_msg[9], 1);
     }
 
     // Disable the data collection
-    enqueue_cmd(&set_auto_data_col_enable_cmd, 0, 0);
+    enqueue_cmd(&set_auto_data_col_enable_cmd, CMD_OBC_HK, 0);
     execute_next_cmd();
-    ASSERT_FALSE(trans_tx_dec_avail);
+    ASSERT_TRUE(trans_tx_dec_avail);
     ASSERT_FALSE(obc_hk_auto_data_col.enabled);
-    enqueue_cmd(&get_auto_data_col_enable_cmd, 1, 0);
+    enqueue_cmd(&get_auto_data_col_enable_cmd, CMD_OBC_HK, 0);
     execute_next_cmd();
     ASSERT_TRUE(trans_tx_dec_avail);
     if (trans_tx_dec_avail) {
-        ASSERT_EQ(trans_tx_dec_len, 1);
-        ASSERT_EQ(trans_tx_dec_msg[0], 0);
+        ASSERT_EQ(trans_tx_dec_len, 10);
+        ASSERT_EQ(trans_tx_dec_msg[9], 0);
     }
 
     enqueue_cmd(&read_prim_cmd_blocks_cmd, 0, 5);
     execute_next_cmd();
     ASSERT_TRUE(trans_tx_dec_avail);
     if (trans_tx_dec_avail) {
-        ASSERT_EQ(trans_tx_dec_len, 5*19);
-        if (trans_tx_dec_len == 5*19) {
+        ASSERT_EQ(trans_tx_dec_len, 5*19+9);
+        if (trans_tx_dec_len == 5*19+9) {
             // TODO: Figure out how to check that the commands match up
         }
     }
@@ -122,39 +124,39 @@ void basic_commands_test (void) {
  */
 void data_collection_test(void) {
     int curr_block_num = 10;
-    enqueue_cmd(&set_cur_block_num_cmd, 1, 10);
-    enqueue_cmd(&get_cur_block_num_cmd, 1, 0);
+    enqueue_cmd(&set_cur_block_num_cmd, CMD_OBC_HK, 10);
+    enqueue_cmd(&get_cur_block_num_cmd, CMD_OBC_HK, 0);
     
     execute_next_cmd();
-    ASSERT_FALSE(trans_tx_dec_avail);
+    ASSERT_TRUE(trans_tx_dec_avail);
     
     execute_next_cmd();
     ASSERT_TRUE(trans_tx_dec_avail);
     if (trans_tx_dec_avail) {
-        ASSERT_EQ(trans_tx_dec_len, 4);
-        if (trans_tx_dec_len == 4) {
-            ASSERT_EQ(trans_tx_dec_msg[0], 10);
-            ASSERT_EQ(trans_tx_dec_msg[1], 0);
-            ASSERT_EQ(trans_tx_dec_msg[2], 0);
-            ASSERT_EQ(trans_tx_dec_msg[3], 0);
+        ASSERT_EQ(trans_tx_dec_len, 13);
+        if (trans_tx_dec_len == 13) {
+            ASSERT_EQ(trans_tx_dec_msg[9], 0);
+            ASSERT_EQ(trans_tx_dec_msg[10], 0);
+            ASSERT_EQ(trans_tx_dec_msg[11], 0);
+            ASSERT_EQ(trans_tx_dec_msg[12], 10);
         }
     }
 
     // Collect a data block and make sure block number increments
-    enqueue_cmd(&col_data_block_cmd, 0, 0);
+    enqueue_cmd(&col_data_block_cmd, CMD_OBC_HK, 0);
     execute_next_cmd();
     ASSERT_TRUE(trans_tx_dec_avail);
     if (trans_tx_dec_avail) {
-        ASSERT_EQ(trans_tx_dec_len, 4);
+        ASSERT_EQ(trans_tx_dec_len, 13);
     }
     ++curr_block_num;   // The block number should increase with a read
 
-    enqueue_cmd(&get_cur_block_num_cmd, 0, 0);
+    enqueue_cmd(&get_cur_block_num_cmd, CMD_OBC_HK, 0);
     execute_next_cmd();
     ASSERT_TRUE(trans_tx_dec_avail);
     if (trans_tx_dec_avail) {
-        ASSERT_EQ(trans_tx_dec_len, 4);
-        ASSERT_EQ(trans_tx_dec_msg[0], curr_block_num);
+        ASSERT_EQ(trans_tx_dec_len, 13);
+        ASSERT_EQ(trans_tx_dec_msg[12], curr_block_num);
     }
 }
 
@@ -171,55 +173,48 @@ void mem_commands_test(void) {
     enqueue_cmd(&set_mem_sec_start_addr_cmd, 1, valid_start);
     enqueue_cmd(&set_mem_sec_end_addr_cmd, 1, valid_end);
     execute_next_cmd();
-    ASSERT_FALSE(trans_tx_dec_avail);
+    ASSERT_TRUE(trans_tx_dec_avail);
     ASSERT_EQ(obc_hk_mem_section.start_addr, 0x3e8);
     execute_next_cmd();
-    ASSERT_FALSE(trans_tx_dec_avail);
+    ASSERT_TRUE(trans_tx_dec_avail);
     ASSERT_EQ(obc_hk_mem_section.end_addr, 0x7d0);
 
 
     // Get the memory addresses
-    enqueue_cmd(&get_mem_sec_start_addr_cmd, 1, 0);
+    enqueue_cmd(&get_mem_sec_start_addr_cmd, CMD_OBC_HK, 0);
     execute_next_cmd();
     ASSERT_TRUE(trans_tx_dec_avail);
     if (trans_tx_dec_avail) {
-        ASSERT_EQ(trans_tx_dec_len, 8);
-        if (trans_tx_dec_len == 8) {
-            ASSERT_EQ(trans_tx_dec_msg[0], 8);
-            ASSERT_EQ(trans_tx_dec_msg[1], 0xe);
-            ASSERT_EQ(trans_tx_dec_msg[2], 3);
-            for (int i = 3; i < 8; ++i) {
-                ASSERT_EQ(trans_tx_dec_msg[i], 0);
-            }
-        }
+        ASSERT_EQ(trans_tx_dec_len, 13);
+        ASSERT_EQ(trans_tx_dec_msg[9], 0);
+        ASSERT_EQ(trans_tx_dec_msg[10], 0);
+        ASSERT_EQ(trans_tx_dec_msg[11], 3);
+        ASSERT_EQ(trans_tx_dec_msg[12], 0xe8);
+
     }
 
-    enqueue_cmd(&get_mem_sec_end_addr_cmd, 1, 0);
+    enqueue_cmd(&get_mem_sec_end_addr_cmd, CMD_OBC_HK, 0);
     execute_next_cmd();
     ASSERT_TRUE(trans_tx_dec_avail);
     if (trans_tx_dec_avail) {
-        ASSERT_EQ(trans_tx_dec_len, 8);
-        if (trans_tx_dec_len == 8) {
-            ASSERT_EQ(trans_tx_dec_msg[0], 0);
-            ASSERT_EQ(trans_tx_dec_msg[1], 0xd);
-            ASSERT_EQ(trans_tx_dec_msg[2], 7);
-            for (int i = 3; i < 8; ++i) {
-                ASSERT_EQ(trans_tx_dec_msg[i], 0);
-            }
-        }
+        ASSERT_EQ(trans_tx_dec_len, 13);
+        ASSERT_EQ(trans_tx_dec_msg[9], 0);
+        ASSERT_EQ(trans_tx_dec_msg[10], 0);
+        ASSERT_EQ(trans_tx_dec_msg[11], 7);
+        ASSERT_EQ(trans_tx_dec_msg[12], 0xd0);
     }
 
     // Try setting the memory addresses to invalid address, it shouldn't change
     // Set the end address to be before the start
     enqueue_cmd(&set_mem_sec_end_addr_cmd, 1, 100);
     execute_next_cmd();
-    ASSERT_FALSE(trans_tx_dec_avail);
+    ASSERT_TRUE(trans_tx_dec_avail);
     ASSERT_EQ(obc_hk_mem_section.end_addr, 0x7d0);
     
     // Set the start address to an out of range address
-    enqueue_cmd(&set_mem_sec_start_addr_cmd, 1, 128000);
+    enqueue_cmd(&set_mem_sec_start_addr_cmd, 1, 0x600001);  // TODO: Command implementation needs to be fixed to check for out of bounds
     execute_next_cmd();
-    ASSERT_FALSE(trans_tx_dec_avail);
+    ASSERT_TRUE(trans_tx_dec_avail);
     ASSERT_EQ(obc_hk_mem_section.start_addr, 0x3e8);
 }
 
@@ -227,7 +222,7 @@ test_t t1 = { .name = "basic commands test", .fn = basic_commands_test };
 test_t t2 = { .name = "data collection test", .fn = data_collection_test };
 test_t t3 = { .name = "memory commands test", .fn = mem_commands_test };
 
-test_t* suite[] = {&t1, &t2};
+test_t* suite[] = {&t1, &t2, &t3};
 
 int main( void ) {
     init_obc_core();
